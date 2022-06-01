55eaae3350a5f0e5d5362517d7c53a09ab8babca
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2022-06-01 03:27:31 -0700

    Add files via upload
    done chapter 9 to 13 assighment

---
+++ syalani_javascript_assigment/SAYLANI-JS-PROJECTS/chapter-9-11-assignment-js/app.js
@@ -0,0 +1,180 @@
+// queston 1
+// document.write("<h3>Question number 1:</h3>")
+
+// var city = prompt("Enter your city name");
+
+// if (city === "karachi") {
+//     document.write("“Welcome to city of lights”")
+// }
+// else {
+//     document.write("you'r not from karachi")
+// }
+
+// queston 2
+// document.write("<h3>Question number 2:</h3>")
+
+// var gender = prompt("Enter your gender");
+
+// if(gender === "male"){
+// document.write(" Good Morning Sir")
+// }
+//  else if( gender === "female"){
+// document.write("Good Morning Maam");
+//  }
+// else{
+//     document.write("enter a proper gender");
+// }
+
+// queston 3
+// document.write("<h3>Question number 3:</h3>")
+
+// var clr = prompt ("Enter a color of Road Rraffic signal in capital  ");
+
+// if (clr === "Red"){
+//     document.write("<B>Must Stop</B>");
+// }
+// else if( clr === "Yellow"){
+//     document.write("<b>Ready To Move </b>");
+// }
+// else if ( clr === "Green"){
+// document.write("<b>Move Now </b>");
+// }
+// else{
+//     alert("Enter a traffic signals color");
+// }
+
+
+// queston 4
+// document.write("<h3>Question number 4:</h3>")
+
+// var fuel = prompt("Please Enter a Fuel in litres ");
+
+// if( fuel <= "0.25"){
+//     document.write(" “Please refill the fuel in your car”")
+
+// }
+// else{
+//     alert("OK , Sir alright your car is full of fuel")
+// }
+
+// question 6
+// document.write("<h1>Student Marks Sheet</h1>")
+// var total = (300);
+// document.write("<b>Total marks:</b>" + " " + total + "<br>");
+
+// var obt = +prompt(" Enter your Marks");
+// document.write("<b>Marks obtained:</b>" + " " + obt + "<br>");
+
+// var percentage = ((obt * 100) / total);
+// document.write( "<b>Percentage</b>:"  +   percentage + "<BR>");
+
+// var gradeA = ("A-ONE");
+// var gradeA = ("A");
+// var gradeB = ("B");
+// var gradeC = ("Fail");
+
+// if(percentage >=80 ){
+// document.write("<b>Grade: A-one</b><br>");
+// document.write("<b>Remarks: Exellent</b>")
+// }
+// else if (percentage  >=70) {
+//     document.write("<b>Grade: A</b><br>");
+//     document.write("<b>Remarks:Good</b>")
+// }
+// else if( percentage  >=60){
+//     document.write("<b>Grade: B</b><br>");
+//     document.write("<b>Remarks:You Need To Improve</b>")
+// }
+// else if(percentage < 50){
+//     document.write("<b>Grade: Fail</b><br>");
+//     document.write("<b>Remarks:Sorry</b>")
+// }
+// else{
+//     alert("Please Enter A Proper Marks");
+// }
+
+
+// quetion 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 5
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+// var htmlcss = prompt("do you know html or css")
+// var expjs = +prompt("how many exp do you have in js?")
+// var knwJs = prompt("do you know about js?")
+// var age = +prompt("how old are you ?");
+// var res = prompt("do you live in karachi?")
+
+
+// if(htmlcss==="yes"){
+
+//     if(expjs>=2 && knwJs==="yes"){
+
+//         if(age>=18 && res==="karachi"){
+//             alert("you areeligible")
+//         }
+//         else{
+//             alert("you are not eligible")
+//         }
+
+//     }
+//     else{
+//         alert("you are not eligible")
+//     }
+// }
+// else{
+//     alert("you are not eligible")
+// }
