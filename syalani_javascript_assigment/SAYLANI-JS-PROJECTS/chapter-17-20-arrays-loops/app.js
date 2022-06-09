9036e5e3d9de9efbd07489c74788161a0fa423e8
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2022-06-09 06:08:12 -0700

    Add files via upload

---
+++ syalani_javascript_assigment/SAYLANI-JS-PROJECTS/chapter-17-20-arrays-loops/app.js
@@ -0,0 +1,63 @@
+// quetion 1
+
+// var arr = [[0,1,2,3],[1,0,1,2,7,8,6,4],[2,1,0,1]];
+// console.log(arr);
+
+// question 2
+// document.write("<h1>Answer 1<h/>")
+
+// var arr = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+// for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < arr[i].length; j++) {
+//         document.write(arr[i][j])
+//     }
+//     document.write("</br>")
+// }
+// document.write("</br>")
+
+
+// question 3
+// document.write("<h1>Answer 3</h1>")
+
+// for (i = 1; i <= 10; i++) {
+//     document.write("<br>" + i);
+// }
+
+// practice question number 3 if user give number 
+
+// var userNum = +prompt ("Enter number");
+
+// for(i = 1; i < userNum; i++){
+//     document.write("<br>" + i);
+//     }
+
+
+// question 4
+
+// var tableNum1 = +prompt ("Enter a table number");
+// var tableNum2 = +prompt ("Enter table lines");
+
+// for( var i=1; i<=tableNum2; i++ ){
+//     document.write(tableNum1 + "X" + " " + i + " = " + tableNum1 * i + "<br>")
+// }
+
+
+// question 5
+
+//   fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+// for(var i=0; i<fruits.length; i++){
+//     document.write(fruits[i]+ "<br>")
+
+// }
+
+// question 5
+
+
+
+
+
+
+
+
