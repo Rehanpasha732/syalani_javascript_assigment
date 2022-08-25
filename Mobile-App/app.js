d6c39ee0aba27624ef55d2380a197e9e0e97f5d6
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2022-08-24 23:46:16 -0700

    mobile-app

---
+++ Mobile-App/app.js
@@ -0,0 +1,28 @@
+var mobiles = {
+    redmi: {
+        RedmiNote: {
+            memory: '(4GB-128GB)',
+
+
+        },
+
+
+    },
+
+
+}
+var srch = document.getElementById("srch")
+
+
+function search(params) {
+    console.log(mobiles.redmi)
+    if (search === RedmiNote) {
+        alert("hello")
+    }
+    else {
+
+    }
+}
+
+
+
